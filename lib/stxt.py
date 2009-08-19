e5175f8a6dbac4df732cd32bdde9df2b8bb7d6de
unknown <fhopecc@.(none)>
2009-08-19 23:32:51 +0800

    TODO: Parser will make parse tree

--- lib/stxt.py
+++ lib/stxt.py
@@ -1,147 +1,38 @@
+# coding=utf-8
 from __future__ import with_statement
 import os, re, unittest
 class Token:
-  def __init__(self, token, lexeme, value, pos, len):
+  def __init__(self, lexer, file, token, lexeme, value, pos, len):
     self.token, self.lexeme, self.value = token, lexeme, value
-    self.pos, self.len = pos, len
-  def error_msg(self):
-    return self.token+'('+str(self.pos)+')'+ \
-        '['+self.lexeme.decode('utf8').encode('cp950')+']'
-class ParseTreeNode:
-  def __init__(self, type, token):
-    self.type, self.token = type, token
-#
-# book     = book sect1
-# sect1    = HEAD1 content1
-# content1 = content1 sect2 | content1 block
-# sect2    = HEAD2 content2
-# content2 = content2 block
-# block    = PARA | code | list EMPTYLINE
-# list     = list LISTITEM | LISTITEM 
-# code     = CODEHEAD CODEBLOCK   
-# ::
-# Removing left recursive
-# ======================= 
-# book     = sect1 book_ | ''
-# book_    = sect1 book_ | ''
-# sect1    = HEAD1 content1
-# content1 = content1_
-# content1_= '' | sect2 content1_ | block content1_ 
-# sect2    = HEAD2 content2
-# content2 = content2_
-# content2_= '' | block content2_
-# block    = PARA | code | list
-# list     = LISTITEM list_ 
-# list_    = '' | LISTITEM list_ 
-# code     = CODEHEAD  CODEBLOCK   
-class Parser:
-  def __init__(self, src):
-    self.src, self.pos = src, 0
-    l = Lexer(src='abcd')
-    l.run()
-    self.tokens = l.tokens
-    self.advance()
-  def advance(self):
-    try:
-      self.lookahead = self.tokens[self.pos]
-      self.pos += 1
-    except:
-      self.lookahead = None
-  def accept(self, token):
-    if self.lookahead.token == token:
-      print 'accept ' + self.lookahead.error_msg()
-      self.advance()
-      return True
-    return False
-  def expect(self, token):
-    if not self.accept(token):
-      raise ValueError("expect " + token + ", but was " + \
-      self.lookahead.error_msg())
-  def parse(self):
-    self.book()
-    return True
-  # book = sect1 book_ | ''
-  def book(self):
-    if self.lookahead == None:
-      return
-    elif self.lookahead.token == 'HEAD1':
-      self.sect1()
-      self.book_()
-  # book_ = sect1 book_ | ''
-  def book_(self):
-    if self.lookahead == None:
-      return
-    elif self.lookahead.token == 'HEAD1':
-      self.sect1()
-      self.book_()
-  # sect1 = HEAD1 content1
-  def sect1(self):
-    self.expect('HEAD1')
-    self.content1()
-  # content1 = sect2 content1_ | block content1_
-  def content1(self):
-    if self.lookahead.token == 'HEAD2':
-      self.sect2()
-      self.content1_()
-    else:
-      self.block()
-      self.content1_()
-  # content1_= sect2 content1_ | block content1_  | ''
-  def content1_(self):
-    if self.lookahead == None:
-      return
-    elif self.lookahead.token == 'HEAD2':
-      self.sect2()
-      self.content1_()
-    else:
-      self.block()
-      self.content1_()
-  # sect2 = HEAD2 content2
-  def sect2(self):
-    if self.expect('HEAD2'):
-      self.content2()
-  # content2 = content2_
-  def content2(self):
-    return self.content2_()
-  # content2_= '' | block content2_
-  def content2_(self):
-    if self.block():
-      return self.content2_()
-    return True
-  # block = PARA | code | list EMPTYLINE
-  def block(self):
-    if self.lookahead.token == 'PARA':
-      self.expect('PARA')
-    elif self.lookahead.token == 'CODEHEAD':
-      self.code()
-    elif self.lookahead.token == 'LISTITEM':
-      self.list()
-      self.expect('EMPTYLINE')
-  # code = CODEHEAD CODEBLOCK   
-  def code(self):
-    self.expect('CODEHEAD')
-    self.expect('CODEBLOCK')
-  # list = LISTITEM list_ 
-  def list(self):
-    self.expect('LISTITEM')
-    self.list_()
-  # list_ = LISTITEM list_ | ''
-  def list_(self):
-    if self.lookahead.token == 'LISTITEM':
-      self.expect('LISTITEM')
-      self.list_()
+    self.lexer, self.file, self.pos, self.len = lexer, file, pos, len
+  def to_str(self):
+    line_num = self.lexer.src.count('\n', 0, self.pos) + 1
+    output =  self.token+'('+str(line_num)+':'+str(self.pos)+') at '
+    output += self.file 
+    output += '{\n'+self.lexeme+'\n}'
+    return output
+  def to_cp950_str(self):
+    return self.to_str().decode('utf8').encode('cp950')
+def tokenize_file(f):
+  fn = f
+  with open(f) as f:
+    l = Lexer(f.read())
+    l.file = fn
+    return l
 class Lexer:
   def __init__(self, src):
-    self.src=src
-    self.grammar=[(re.compile(r'^\S*\n=+\n', re.M), 
+    self.src, self.file = src, '_STRING_'
+    self.grammar=[(re.compile(r'(^.*)\n=+\n', re.M), 
                    self.HEAD1),
-                  (re.compile(r'^code\..*\n', re.M), 
+                  (re.compile(r'(^.*)\n-+\n', re.M), 
+                   self.HEAD2),
+                  (re.compile(r'^code\.(.*)\n', re.M), 
                    self.CODEHEAD),
-                  (re.compile(r'(.+\n)+(^::\n)', re.M), 
+                  (re.compile(r'((.+\n)+)(^::\n\n)', re.M), 
                    self.CODEBLOCK),
-                  (re.compile(r'\* .*\n', re.M), 
+                  (re.compile(r'\* (.*)\n', re.M), 
                    self.LISTITEM),
-                  (re.compile(r'(.+\n)+(\n|\Z)', re.M), 
+                  (re.compile(r'((.+\n)+)(\n|\Z)', re.M), 
                    self.PARA),
                   (re.compile(r'^\s*$\n', re.M), 
                    self.EMPTYLINE),
@@ -149,50 +40,226 @@ class Lexer:
                    self.LEXERROR)
                  ]
   def run(self):
-    with open(r"d:\stxt\stxt\db\timestamp.stx") as f:
-      self.src, self.cur, self.tokens = f.read(), 0, []
-      while self.cur < len(self.src):
-        for p in self.grammar:
-          m = p[0].match(self.src, self.cur)
-          if m:
-            p[1](m)
-            self.cur += len(m.group(0))
-            break
+    self.cur, self.tokens = 0, []
+    while self.cur < len(self.src):
+      for p in self.grammar:
+        m = p[0].match(self.src, self.cur)
+        if m:
+          p[1](m)
+          self.cur += len(m.group(0))
+          break
   def HEAD1(self,m):
     lexeme = m.group(0)
-    tok = Token('HEAD1', lexeme, 0, self.cur+1, len(lexeme))
+    tok = Token(self, self.file, 'HEAD1', lexeme, m.group(1), self.cur+1, len(lexeme))
+    self.tokens.append(tok)
+  def HEAD2(self,m):
+    lexeme = m.group(0)
+    tok = Token(self, self.file, 'HEAD2', lexeme, m.group(1), self.cur+1, len(lexeme))
     self.tokens.append(tok)
   def CODEHEAD(self,m):
     lexeme = m.group(0)
-    tok = Token('CODEHEAD', lexeme, 0, self.cur+1, len(lexeme))
+    tok = Token(self, self.file, 'CODEHEAD', lexeme, m.group(1), self.cur+1, len(lexeme))
     self.tokens.append(tok)
   def CODEBLOCK(self,m):
     lexeme = m.group(0)
-    tok = Token('CODEBLOCK', lexeme, 0, self.cur+1, len(lexeme))
+    tok = Token(self, self.file, 'CODEBLOCK', lexeme, m.group(1), self.cur+1, len(lexeme))
     self.tokens.append(tok)
   def PARA(self,m):
     lexeme = m.group(0)
-    tok = Token('PARA', lexeme, 0, self.cur+1, len(lexeme))
+    tok = Token(self, self.file, 'PARA', lexeme, m.group(1), self.cur+1, len(lexeme))
     self.tokens.append(tok)
   def LISTITEM(self,m):
     lexeme = m.group(0)
-    tok = Token('LISTITEM', lexeme, 0, self.cur+1, len(lexeme))
+    tok = Token(self, self.file, 'LISTITEM', lexeme, m.group(1), self.cur+1, len(lexeme))
     self.tokens.append(tok)
   def EMPTYLINE(self,m):
     lexeme = m.group(0)
-    tok = Token('EMPTYLINE', lexeme, 0, self.cur+1, len(lexeme))
+    tok = Token(self, self.file, 'EMPTYLINE', lexeme, 0, self.cur+1, len(lexeme))
     self.tokens.append(tok)
   def LEXERROR(self,m):
     raise ValueError('LexError at (' + 
           str(self.src[self.cur:]).decode('utf8').encode('cp950')+')')
+class ParseTreeNode:
+  def __init__(self, type, token=None):
+    self.type, self.token, self.children= type, token, []
+    self.title, self.parent = '', None
+    if not token == None:
+      self.value = token.value
+  def append(self, node):
+    node.parent = self
+    self.children.append(node)
+  def isRoot(self):
+    return self.parent == None
+  def height(self):
+    if self.isRoot():
+      return 0
+    c, h = self, 0
+    while c.parent:
+      c = c.parent
+      h += 1
+    return h
+  def print_type_tree(self):
+    print ' ' * self.height() + self.type
+    for c in self.children:
+      c.print_type_tree()
+#
+# book     = sect1s EOF | EOF
+# sect1s   = sect1s sect1 | sect1
+# sect1    = HEAD1 content1
+# content1 = content1 sect2 | content1 block | sect2 | block
+# sect2    = HEAD2 content2
+# content2 = content2 block | block
+# block    = PARA | code | list EMPTYLINE
+# code     = CODEHEAD CODEBLOCK
+# list     = list LISTITEM | LISTITEM 
+# ::
+# Removing left recursive
+# ======================= 
+# book     = sect1s EOF | EOF
+# sect1s   = sect1 sect1s_
+# sect1s_  = sect1 sect1s_ | ''
+# sect1    = HEAD1 content1
+# content1 = sect2 content1_ | block content1_
+# content1_= sect2 content1_ | block content1_ | ''
+# sect2    = HEAD2 content2
+# content2 = block content2_
+# content2_= block content2_ | ''
+# block    = PARA | code | list
+# code     = CODEHEAD  CODEBLOCK   
+# list     = LISTITEM list_ 
+# list_    = LISTITEM list_ | ''
+def parse_file(f):
+ return Parser(tokenize_file(f))
+class Parser:
+  def __init__(self, lexer):
+    self.lexer, self.pos = lexer, 0
+    self.lexer.run()
+    self.tokens = self.lexer.tokens
+    self.tokens.append(Token(self.lexer, self.lexer.file, 'EOF', \
+      '_EOF_', None, len(self.lexer.src),0))
+    self.advance()
+  def has_seen(self, token):
+    if self._lookahead.token == token:
+      return True
+    return False
+  def advance(self):
+    try:
+      self._lookahead = self.tokens[self.pos]
+      self.pos += 1
+    except:
+      self._lookahead = None
+  def match(self, token):
+    if self._lookahead.token == token:
+      print 'match ' + self._lookahead.to_cp950_str()
+      n = ParseTreeNode(token, self._lookahead)
+      #if token != 'EOF': 
+      self.advance()
+      return n
+    raise ValueError("match " + token + ", but was " + \
+    self._lookahead.to_cp950_str())
+  def parse(self):
+    self.tree = self.book()
+    return True
+  # book = sect1s EOF | EOF
+  def book(self):
+    if self.has_seen('HEAD1'):
+      self.sect1s()
+      self.match('EOF')
+    else:
+      self.match('EOF')
+  # sect1s = sect1 sect1s_
+  def sect1s(self):
+    self.sect1()
+    self.sect1s_()
+  # sect1s_ = sect1 sect1s_ | ''
+  def sect1s_(self):
+    if self.has_seen('HEAD1'):
+      self.sect1()
+      self.sect1s_()
+  # sect1 = HEAD1 content1
+  def sect1(self):
+    self.match('HEAD1')
+    self.content1()
+  # content1 = sect2 content1_ | block content1_
+  def content1(self):
+    if self.has_seen('HEAD2'):
+      self.sect2()
+      self.content1_()
+    elif self.has_seen('PARA') or self.has_seen('CODEHEAD') or\
+       self.has_seen('LISTITEM'):
+      self.block()
+      self.content1_()
+  # content1_= sect2 content1_ | block content1_ | ''
+  def content1_(self):
+    if self.has_seen('HEAD2'):
+      self.sect2()
+      self.content1_()
+    elif self.has_seen('PARA') or self.has_seen('CODEHEAD') or\
+       self.has_seen('LISTITEM'):
+      self.block()
+      self.content1_()
+  # sect2 = HEAD2 content2
+  def sect2(self):
+    self.match('HEAD2')
+    self.content2()
+  # content2 = block content2_
+  def content2(self):
+    self.block()
+    self.content2_()
+  # content2_= block content2_ | ''
+  def content2_(self):
+    if self.has_seen('PARA') or self.has_seen('CODEHEAD') or\
+       self.has_seen('LISTITEM'):
+      self.block()
+      self.content2_()
+  # block = PARA | code | list EMPTYLINE
+  def block(self):
+    if self.has_seen('PARA'):
+      self.match('PARA')
+    elif self.has_seen('CODEHEAD'):
+      self.code()
+    elif self.has_seen('LISTITEM'):
+      self.list()
+      self.match('EMPTYLINE')
+  # code = CODEHEAD CODEBLOCK   
+  def code(self):
+    self.match('CODEHEAD')
+    self.match('CODEBLOCK')
+  # list = LISTITEM list_ 
+  def list(self):
+    self.match('LISTITEM')
+    self.list_()
+  # list_ = LISTITEM list_ | ''
+  def list_(self):
+    if self.has_seen('LISTITEM'):
+      self.match('LISTITEM')
+      self.list_()
+def to_html(tree):
+  if tree.type == 'sect1':
+    html =  '<h1>' + tree.title +'</h1>\n'
+  elif tree.type == 'sect2':
+    html =  '<h2>' + tree.title +'</h2>\n'
+  elif tree.type == 'code':
+    html = '<h4>程式碼：' + tree.title +'</h4>\n'
+  elif tree.type == 'CODEBLOCK':
+    html = '<pre>\n' + tree.value + '</pre>\n'
+  elif tree.type == 'PARA':
+    html = '<p>\n' + tree.value + '</p>\n'
+  else:
+    html = ' ' * tree.height() + tree.type + '\n'
+  for c in tree.children:
+    if c.type == 'list':
+      html += '<ul>\n'
+      for i in c.children:
+        html += '<li>' + i.value + '</li>\n'
+      html += '</ul>\n'
+    else:
+      html += to_html(c)
+  return html
 class RTxtLexerTest(unittest.TestCase):
   def testSource(self):
     l = Lexer(src='abcd')
     self.assertEqual('abcd', l.src)
-    #l.run()
-    #for t in l.tokens:
-    #  print t.token+'['+t.lexeme.decode('utf8').encode('cp950')+']'
-    #print 'There are ' + str(len(l.tokens)) + ' tokens.'
 class STXTParserTest(unittest.TestCase):
   def testSource(self):
     p = Parser(src='abcd')
@@ -201,6 +268,16 @@ class STXTParserTest(unittest.TestCase):
     p = Parser(src='abcd')
     self.assert_(p.parse())
 if __name__ == '__main__':
-  p = Parser(src='abcd')
+  #l = tokenize_file(r"d:\stxt\stxt\db\timestamp.stx")
+  #l.run()
+  #for t in l.tokens:
+  #  print t.token+'['+t.lexeme.decode('utf8').encode('cp950')+']'
+  #print 'There are ' + str(len(l.tokens)) + ' tokens.'
+  p = parse_file(r"d:\stxt\stxt\db\timestamp.stx")
   p.parse()
+  #p.tree.print_type_tree()
+  #print to_html(p.tree).decode('utf8').encode('cp950')
+
+  #with open(r'd:\stxt\html\db.html', 'w') as f:
+  #  f.write('<html>\n'+to_html(p.tree)+'</html>')
   #unittest.main()
