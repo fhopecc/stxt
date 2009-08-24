8c36de80d723b1a660d07accbc093c140e38b013
unknown <fhopecc@.(none)>
2009-08-24 22:24:34 +0800

    web outputer prototype ok

--- lib/web_outputter.py
+++ lib/web_outputter.py
@@ -1,5 +1,6 @@
 # coding=utf-8
-import stxt
+from __future__ import with_statement
+import os, re, unittest, stxt
 class Outputter:
   def __init__(self, file):
     p = stxt.parse_file(r"d:\stxt\stxt\db\concurrent_control.stx")
@@ -10,7 +11,16 @@ class Outputter:
 class WEBOutputter(Outputter):
   def to_web(self):
     for sect1 in self.doctree.children:
-      print self.to_html(sect1).decode('utf8').encode('cp950')
+      t = ''
+      tfn = r'd:\stxt\template\web_section.html'
+      with open(tfn) as f:
+        t = f.read()
+      fn = r'd:\stxt\public\db\%s.html' % sect1.section_number() 
+      with open(fn, 'w') as f:
+        f.write(t % 
+            {'title': sect1.title, 
+             'content':self.to_html(sect1)
+            })
   def to_html(self, tree):
     html = ''
     if tree.type == 'sect1':
@@ -19,10 +29,11 @@ class WEBOutputter(Outputter):
         html += self.to_html(c)
     elif tree.type == 'sect2':
       html =  '<h2>' + tree.title +'</h2>\n'
+      for c in tree.children:
+        html += self.to_html(c)
     elif tree.type == 'code':
-      html = '<h4>程式碼：' + tree.title +'</h4>\n'
-    elif tree.type == 'CODEBLOCK':
-      html = '<pre>\n' + tree.value + '</pre>\n'
+      html  = '<h4>程式碼：' + tree.title +'</h4>\n'
+      html += '<pre>\n' + tree.children[0].value + '</pre>\n'
     elif tree.type == 'PARA':
       html = '<p>\n' + tree.value + '</p>\n'
     elif tree.type == 'list':
