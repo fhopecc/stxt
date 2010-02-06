3ba7e6662e69dcbfdd2cb11f9c19ed768125863d
fhopecc <fhopecc@gmail.com>
2010-02-06 17:28:31 +0800

    F

--- task/wget.py
+++ task/wget.py
@@ -1,8 +1,15 @@
 # coding=utf-8
 from __future__ import with_statement
-from urlparse import urlparse
-import sys, os, urllib
+from urllib import parse
+from logging import config
+import sys, os, urllib, logging
+
+config.fileConfig(r'task\log.conf')
+logger = logging.getLogger()
+
 def wget(url, file=None):
+    '將指定 URL 之資源下載至本機檔案\n'
+    logger.info('開始下載 %s ' % url)
     bf = os.path.basename(urlparse(url).path)
     if not file: file = bf
     r = urllib.urlopen(url)
@@ -10,9 +17,9 @@ def wget(url, file=None):
         f.write(r.read())
 
 def usage():
-    usage =  u'將指定 URL 之資源下載至本機檔案\n'
-    usage += u'用法: %s url [file]\n'
-    usage += u'file: 指定資源所要存成本機檔案之檔名\n'
+    usage =  wget.__doc__
+    usage += '用法: %s url [file]\n'
+    usage += 'file: 指定資源所要存成本機檔案之檔名\n'
     return usage % os.path.basename(sys.argv[0]) 
 
 if __name__ == '__main__':
@@ -23,4 +30,4 @@ if __name__ == '__main__':
         elif len(sys.argv) == 3:
             wget(sys.argv[1], sys.argv[2])
     except IndexError:
-        print usage()
+        logger.info(usage())
