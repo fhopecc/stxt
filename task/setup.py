9329cf3d00acc302c5422e5a022dda9c8af09585
unknown <fhopecc@.(none)>
2010-09-05 10:09:38 +0800

    modify some document

--- task/setup.py
+++ task/setup.py
@@ -1,4 +1,4 @@
-from os import path
+# coding=utf8
 import sys, os
 
 def set_sysenv(var, val):
@@ -7,27 +7,31 @@ def set_sysenv(var, val):
         raise 'set_sysenv("%s", "%s") failed!' % (var, val)
 
 def add_path(p):
-    if p not in os.environ['PATH'].split(';'):
-        set_sysenv('PATH', r"%PATH%;" + p)
-        print 'Add path "%s"' % p
+    ps = os.environ['PATH'].split(';')
+    if p not in ps:
+        ps.append(p)
+        #import pdb;pdb.set_trace()
+        ps = list(set(ps))
+        set_sysenv('PATH', ';'.join(ps))
+        print 'Append "%s" to PATH.' % p
+
+#add_path(os.path.dirname(sys.argv[0]))
+
+#add_path(r'C:\Program Files\GnuWin32\bin')
+
 
 '''
-	def uniq_path
-		upaths = ENV['PATH'].split(/;/).uniq 
-    set_sysenv 'PATH', "#{upaths.join(';')}"
-	  puts "PATH has unique its elements successfully!"
-	end
-
-	def del_path path
-		paths = ENV['PATH'].split(/;/)
-		if paths.include? path
-			paths.delete path
-      set_sysenv 'PATH', "#{paths.join(';')}"
-			puts "Path [#{path}] has removed from PATH envvar successfully!"
-		else
-			puts "Path [#{path}] is not in PATH envvar!"
-		end
-	end
+0.1:將路徑加入程式搜尋路徑 environ['PATH']
 '''
-add_path(path.dirname(sys.argv[0]))
-add_path(r'C:\Program Files\GnuWin32\bin')
+if __name__ == "__main__":
+    from optparse import OptionParser
+    usage = u"usage: %prog log [options]"
+    parser = OptionParser(usage, version="%prog 0.1", 
+             description=u"環境設定自動化工具組"
+        )
+    parser.add_option("-p", "--path", dest="path", 
+                       help=u"將路徑加入程式搜尋路徑 environ['PATH']")
+
+    (options, args) = parser.parse_args()
+
+    if options.path: add_path(options.path)
