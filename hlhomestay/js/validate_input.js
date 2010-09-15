4b6c3bf967ead7fef550fcc6ec04bd7b563ab3d6
fhopecc <fhopecc@gmail.com>
2010-09-15 17:59:26 +0800

    abcd

---
+++ hlhomestay/js/validate_input.js
@@ -0,0 +1,17 @@
+/**--------------------------
+//* Validate Date Field script- By JavaScriptKit.com
+//* For this script and 100s more, visit http://www.javascriptkit.com
+//* This notice must stay intact for usage
+---------------------------**/
+
+function validate_email(field,alerttxt)
+{
+with (field)
+  {
+  apos=value.indexOf("@");
+  dotpos=value.lastIndexOf(".");
+  if (apos<1||dotpos-apos<2)
+    {alert(alerttxt);return false;}
+  else {return true;}
+  }
+}
