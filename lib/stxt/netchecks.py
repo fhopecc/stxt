ebecf1ba1cf9d77dc04f7e18dac36e5a90042eb1
fhopecc <fhopecc@gmail.com>
2010-12-28 17:30:27 +0800

    abcd

--- lib/stxt/netchecks.py
+++ lib/stxt/netchecks.py
@@ -181,7 +181,7 @@ if __name__ == '__main__':
                        help=u"指定輸入之字串編碼")
 
     oparser.add_option("-d", "--date", dest="date", 
-                       help=u"列出指定日期之日誌，未指定則為昨天，日期格式例子：12-17-2010")
+                       help=u"列出指定日期之日誌，未指定則為昨天，日期格式例子：20101217")
 
     oparser.add_option("-o", "--output", dest="output", 
                        default='test.html',
