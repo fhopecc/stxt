4b5319e8a6ab45394d88792b0969e64b2b7e8565
fhopecc <fhopecc@gmail.com>
2011-05-04 17:28:58 +0800

    abcd

--- task/backup_onms.py
+++ task/backup_onms.py
@@ -4,9 +4,12 @@ import sys, os
 
 pg_dump_path = r'"c:/Program Files/PostgreSQL/9.0/bin/pg_dump.exe"'
 
-def backup_onms(host):
-    fp = fpath(host)
-    cmd = '%s -h %s -U opennms -f %s opennms' % (pg_dump_path, host, fp)
+def backup_onms(options):
+    fp = fpath(options)
+    #import pdb; pdb.set_trace()
+    cmd = '%s -h %s -U opennms -f %s opennms' % (pg_dump_path, 
+                                                 options.host,
+                                                 fp)
     os.system(cmd)
     zipfile(fp)
     print u'資料庫已備份至 %s' % fp + '.zip'
@@ -17,24 +20,32 @@ def zipfile(fn):
     zf.write(fn)
     zf.close()
 
-def fpath(host):
+def fpath(options):
     import datetime
     today = datetime.date.today()
-    return 'onmsdb_%s_%s' % (host.replace('.', '_'), today.strftime('%Y%m%d')) 
+    return '%sonmsdb_%s_%s' % (options.dir, 
+                               options.host.replace('.', '_'), 
+                               today.strftime('%Y%m%d')) 
 
 # 0.1: 可備出 onms 之 DB
+# 0.2: 加入 dir 選項，指定備出檔案存放之目錄
 if __name__ == "__main__":
     from optparse import OptionParser
     usage = u"usage: %prog [options]"
-    oparser = OptionParser(usage, version="%prog 0.1", 
-                          description=u"備份ONMS設定"
-             )
+    oparser = OptionParser(usage, version="%prog 0.2", 
+                           description=u"備份ONMS設定，"\
+                                       u"用戶端必須安裝 pgsql。"
+              )
 
     oparser.add_option("-s", "--host", dest="host", 
                        default='192.168.1.13',
                        help=u"指定備出之來源設備 IP")
 
-    oparser.add_option("-c", "--short_cut", dest="short_cut", 
+    oparser.add_option("-d", "--dir", dest="dir", 
+                       default='',
+                       help=u"指定備出檔案存放之目錄，預設為空字串")
+
+    oparser.add_option("-H", "--short_cut", dest="short_cut", 
                        choices=['outer', 'inner'],
                        help=u"使用捷徑" \
                             u"inner 表內網、outer 表外網")
@@ -43,9 +54,10 @@ if __name__ == "__main__":
 
     if options.short_cut:
         # options short cut
+        options.dir = "bak\\"
         if options.short_cut == 'outer':
             options.host  = '192.168.1.13'
         elif options.short_cut == 'inner':
             options.host  = '10.66.4.17'
-    
-    backup_onms(options.host)
+
+    backup_onms(options)
