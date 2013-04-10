ab3564c16d35d1ccecdf149bdaf385f3f40f5f25
fhopecc <fhopecc@gmail.com>
2013-04-10 16:38:05 +0800

    backup onms option add

--- task/backup_netconfig.hs
+++ task/backup_netconfig.hs
@@ -24,12 +24,14 @@ import Text.Printf
 outerONMSIP = "192.168.1.13"
 innerONMSIP = "10.66.4.17"
 
-main = backupONMSDB outerONMSIP
+--main = backupONMSDB outerONMSIP
+main = cmd
 
 cmd = do
     hSetEncoding stdout cp950
     args <- getArgs
     let source = map toUpper. dropWhile (== ' ') . head $ args 
+
     m <- newEmptyMVar
     forkIO $ case source of 
          "FG100A"  -> do backupFG100A 
@@ -50,6 +52,10 @@ cmd = do
             
                          putStrLn "備份 ONMSDB 成功!"
                          putMVar m "備份內網設備成功!"
+         "OONMS"   -> do backupONMSDB outerONMSIP
+                         putMVar m "備份外網ONMS成功!"
+         "IONMS"   -> do backupONMSDB innerONMSIP
+                         putMVar m "備份內網ONMS成功!"
          otherwise -> do showHelp
                          putMVar m ""
         
@@ -292,65 +298,4 @@ backupONMSDB host = do
                                                    "ONMSDB 備份失敗!")
     B.readFile dest >>= (B.writeFile $ dest ++ ".gz") . compress
     removeFile dest
-    putStrLn host ++ "ONMSDB 備份成功!"
-
---def backup_onms(options):
---    fp = fpath(options)
-    --import pdb; pdb.set_trace()
---    cmd = '%s -h %s -U opennms -f %s opennms' % (pg_dump_path, 
---                                                 options.host,
---                                                 fp)
---    os.system(cmd)
---    zipfile(fp)
---    os.system('del %s' % fp)
---    print u'資料庫已備份至 %s' % fp + '.zip'
-
---def zipfile(fn):
---    zfn = fn + '.zip'
---    zf = ZipFile(zfn, 'w', ZIP_DEFLATED)
---    zf.write(fn)
---    zf.close()
-
-
--- def fpath(options):
---     import datetime
- --    today = datetime.date.today()
-  --   return '%sonmsdb_%s_%s' % (options.dir, 
-    --                            options.host.replace('.', '_'), 
-      --                          today.strftime('%Y%m%d')) 
-
--- 0.1: 可備出 onms 之 DB
--- 0.2: 加入 dir 選項，指定備出檔案存放之目錄
--- 0.3: 壓縮完，刪除原始檔案
--- if __name__ == "__main__":
---     from optparse import OptionParser
- --    usage = u"usage: %prog [options]"
---     oparser = OptionParser(usage, version="%prog 0.3", 
---                            description=u"備份ONMS設定，"\
---                                        u"用戶端必須安裝 pgsql。"
---               )
-
---     oparser.add_option("-s", "--host", dest="host", 
---                        default='192.168.1.13',
---                        help=u"指定備出之來源設備 IP")
-
---     oparser.add_option("-d", "--dir", dest="dir", 
---                        default='',
---                        help=u"指定備出檔案存放之目錄，預設為空字串")
-
---     oparser.add_option("-H", "--short_cut", dest="short_cut", 
---                        choices=['outer', 'inner'],
---                        help=u"使用捷徑" \
---                             u"inner 表內網、outer 表外網")
-
---     (options, args) = oparser.parse_args()
-
---     if options.short_cut:
-  --       # options short cut
---         options.dir = "bak\\"
-    --     if options.short_cut == 'outer':
-  --           options.host  = '192.168.1.13'
-      --   elif options.short_cut == 'inner':
-        --     options.host  = '10.66.4.17'
-
---     backup_onms(options)
+    putStrLn $ host ++ "ONMSDB 備份成功!"
