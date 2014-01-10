4581f597f4968440b5e22a5c90e1f3f988fc8bf5
fhopecc <fhopecc@gmail.com>
2014-01-10 09:55:30 +0800

    parse vbsscript ok

--- dochandle/stxt.hs
+++ dochandle/stxt.hs
@@ -3,6 +3,7 @@ module STXT( run
            , code
            , content, contents
            , sect2title
+           , sect1, sect1s
            , sect2, sect2s
            ) 
 where
@@ -11,6 +12,9 @@ import Text.ParserCombinators.Parsec
 import System.Environment
 import System.IO
 import Control.Monad
+
+data Sect1    = Sect1 Title Sect2s
+                deriving (Show)
 
 data Sect2    = Sect2 Title Contents
                 deriving (Show)
@@ -22,52 +26,63 @@ data Content  = Para Lines
 type Title    = String
 type Lines    = [String]
 type Contents = [Content]
+type Sect2s   = [Sect2]
 
-sect2s = sect2 `sepEndBy` (char '\n') 
+--sect1s = sect1 `sepEndBy` (char '\n') 
+sect1s = many sect1
 
-line :: Parser String
-line = do head <- noneOf "=-\n"
-          tail <- try (manyTill (noneOf "\n") 
-                                (lookAhead (string "碼：\n\n")))
-              <|> (many $ noneOf "\n")
-          return $ head:tail
+sect1 :: Parser Sect1
+sect1 = do
+    t  <- sect1title
+    cs <- sect2s 
+    return $ Sect1 t cs 
+
+sect1title :: Parser Title
+sect1title = do
+    title <- many $ noneOf "\n"; char '\n'
+    skipMany1 $ char '='; string "\n\n"
+    return title
+
+--sect2s = sect2 `sepEndBy` (char '\n') 
+sect2s = many sect2
+
+sect2title :: Parser Title
+sect2title = do
+    title <- many1 $ noneOf "\n"; char '\n'
+    skipMany1 $ char '-'; string "\n\n"
+    return title
+
+sect2 :: Parser Sect2
+sect2 = do notFollowedBy sect1title
+           t  <- sect2title
+           cs <- contents 
+           return $ Sect2 t cs 
+
+contents = many content
+
+content =  code
+       <|> (do p <- para
+               many $ char '\n'
+               return p
+           )
+
+code :: Parser Content
+code = do string "碼：\n\n"
+          src <- manyTill anyChar (try (string "\n\n\n"))
+          return $ Code src
 
 para :: Parser Content
 para  = do ls <- line `sepEndBy1` (char '\n')
            return $ Para ls
 
-code :: Parser Content
-code = do string "碼：\n\n"
-          src <- manyTill anyChar (try (string "\n\n"))
-          return $ Code src
-
-content =  code
-       <|> para 
-        
-contents = many content
-
-sect2title :: Parser Title
-sect2title = do
-    title <- line; char '\n'
-    skipMany1 $ char '-'; char '\n'
-    return title
-
-
-
-
-sect2 :: Parser Sect2
-sect2 = do
-    t  <- sect2title
-    cs <- contents 
-    return $ Sect2 t cs 
-
-sect1title :: Parser Title
-sect1title = do
-    title <- line; char '\n'
-    skipMany1 $ char '-'; char '\n'
-    return title
-
-
+line :: Parser String
+line = do notFollowedBy sect1title
+          notFollowedBy sect2title  
+          head <- noneOf "\n"
+          tail <- try (manyTill (noneOf "\n") 
+                                (lookAhead (string "碼：\n\n")))
+              <|> (many $ noneOf "\n")
+          return $ head:tail
 
 run p input
     = case (parse p "" input) of
@@ -78,6 +93,7 @@ main = do
     args <- getArgs 
     let src  = args !! 0
     f <- openFile src ReadMode
+    hSetEncoding f utf8
     c <- hGetContents f
-    let o = run sect2s c
+    let o = run sect1s c
     putStr $ o
--- dochandle/testStxt.hs
+++ dochandle/testStxt.hs
@@ -22,35 +22,57 @@ testLine = test [ "testLine" ~:
         , "Para [\"l1\",\"l2\\30908\\65306\"]" ~=?  run para "l1\nl2碼：\n"
         , "Para [\"l1\"]" ~=?  run para "l1碼：\n\nl2"
         ]
+    ,  "test contents" ~: 
+        [ "[Para [\"l1\"],Para [\"l2\"]]" ~=?  
+          run contents "l1\n\nl2\n"
+        ]
     ]
     ]
 
 testParser = test [ "testParser" ~: 
     [ "test code" ~: 
-        [ "Code \"c d\\nc d 2\\nc d 3 \""~=?  run code "碼：\n\nc d\nc d 2\nc d 3 \n\n"
+        [ "Code \"c d\\nc d 2\\nc d 3 \""~=? 
+          run code "碼：\n\nc d\nc d 2\nc d 3 \n\n\n"
         ]
 
     , "test sect2title" ~: 
-        [ "\"s2\"" ~=?  run sect2title "s2\n-----------\n"
+        [ "\"s2\"" ~=?  run sect2title "s2\n-----------\n\n"
         ]
-
     , "test sect2" ~: 
-        [ "Sect2 \"s2\" []" ~=?  run sect2 "s2\n-----------\n"
-        , "Sect2 \"s2\" [Para [\"p1\"]]" ~=?  run sect2 "s2\n-----------\np1"
+        [ "Sect2 \"s2\" []" ~=?  run sect2 "s2\n--\n\n"
+        , "Sect2 \"s2\" [Para [\"p1\"]]" ~=?  run sect2 "s2\n--\n\np1"
         , "Sect2 \"s2\" [Para [\"p1\"],Code \"c\"]" ~=?  
-          run sect2 "s2\n-----------\np1碼：\n\nc\n\n"
+          run sect2 "s2\n--\n\np1碼：\n\nc\n\n\n"
 
         , "Sect2 \"s2\" [Para [\"p1\"],Code \"c\",Para [\"p2\"]]" ~=?  
-          run sect2 "s2\n-----------\np1碼：\n\nc\n\np2"
+          run sect2 "s2\n--\n\np1碼：\n\nc\n\n\np2"
         ]
     , "test sect2s" ~: 
-        [ "[Sect2 \"s2\" []]" ~=?  run sect2s "s2\n--\n"
-        , "[Sect2 \"s2\" [Para [\"p1\"]]]" ~=?  run sect2s "s2\n--\np1"
+        [ "[Sect2 \"s2\" []]" ~=?  run sect2s "s2\n--\n\n"
+        , "[Sect2 \"s2\" [Para [\"p1\"]]]" ~=?  run sect2s "s2\n--\n\np1"
         , "[Sect2 \"t1\" [Para [\"p1\"]],Sect2 \"t2\" "
           ++ "[Para [\"p1\"],Code \"c\"]]" 
-          ~=?  run sect2s "t1\n--\np1\n\nt2\n--\np1碼：\n\nc\n\n"
+          ~=?  run sect2s "t1\n--\n\np1\nt2\n--\n\np1碼：\n\nc\n\n\n"
+        , "[Sect2 \"t1\" [Para [\"l1\",\"l2\"]],Sect2 \"t2\" "
+          ++ "[Para [\"l1\"]]]" 
+          ~=?  run sect2s "t1\n--\n\nl1\nl2\nt2\n--\n\nl1"
         ]
+    , "test sect1" ~: 
+        [ "Sect1 \"s1\" [Sect2 \"s2\" []]" ~=? 
+          run sect1 "s1\n==\n\ns2\n--\n\n"
+        , "Sect1 \"s1\" [Sect2 \"s2\" [Para [\"l1\",\"l2\"],Code \"c1\\nc2\\n c3\"]]" ~=? 
+          run sect1 "s1\n==\n\ns2\n--\n\nl1\nl2碼：\n\nc1\nc2\n c3\n\n\n"
+        ]
+    , "test sect1s" ~: 
+        [ "[Sect1 \"s1\" [Sect2 \"s2\" []]]" ~=? 
+          run sect1s "s1\n==\n\ns2\n--\n\n"
+        , "[Sect1 \"s1\" [Sect2 \"t1\" [Para [\"l1\"]]],Sect1 \"s2\" []]" ~=? 
+          run sect1s "s1\n==\n\nt1\n--\n\nl1\n\ns2\n==\n\n"
 
- 
+        ,  "[Sect1 \"c1\" [Sect2 \"t1\" [Para [\"l1\"]]],"
+       ++  "Sect1 \"c2\" []]" 
+       ~=? 
+          run sect1s "c1\n==\n\nt1\n--\n\nl1\n\nc2\n==\n\n"
+        ]
     ]
     ]
