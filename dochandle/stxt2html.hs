d3019bb636a05f9557c9d0d7b8ac61376988d278
unknown <fhopecc@gmail.com>
2014-02-27 15:35:53 +0800

    stxt2html: restructure with State monad

--- dochandle/stxt2html.hs
+++ dochandle/stxt2html.hs
@@ -9,11 +9,6 @@ import Data.List
 import Data.Maybe
 import System.Directory
 
-data Env = Env { getSource :: FilePath  
-               , getOutDir :: FilePath  
-               , getDoc    :: STXT.Doc
-               }
-
 data Page = Page { pSource  :: FilePath
                  , pOutDir  :: FilePath
                  , pDoc     :: STXT.Doc
@@ -27,9 +22,9 @@ data Page = Page { pSource  :: FilePath
 
 siteUrl = "http://fhopehltb.appspot.com"
 siteName = "剛的網站"
-webdir = "d:\\stxt\\fhopecc\\www"
+webdir = "d:\\fhopehltb\\"
 webcss = "d:\\stxt\\dochandle\\web.css"
-index  = "d:\\stxt\\fhopecc\\www\\index\\index.html"
+index  = FP.combine webdir "index.html"
 
 main = do 
     --let src = "d:\\stxt\\doc\\vbscript\\vbscript.txt"
@@ -45,18 +40,18 @@ main = do
     c <- hGetContents f
     copyFile webcss (FP.combine outDir "web.css")
     putStrLn "copy web.css"
-    mapM_ writeHtml (runState getHtmls (Page  { getSource = src
-                                             , getOutDir = outDir
-                                             , getDoc = STXT.run c
-                                             }))
-    copyFile index "d:\\stxt\\fhopecc\\www\\index.html"
-
-getHtmls :: State Page [(FilePath, Html)]
-getHtmls = do
-    ihtml <- askIndexHtml
-    s1htmls <- askSect1Htmls  
-    s2htmls <- askSect2Htmls
-    return $ ihtml : s1htmls ++ s2htmls
+    mapM_ writeHtml (evalState getHtmls (Page { pSource  = src
+                                              , pOutDir  = outDir
+                                              , pDoc     = STXT.run c
+                                              , pTitle   = ""
+                                              , pContent = []
+                                              , pSect1s  = []
+                                              , pSect1   = Nothing
+                                              , pSect2s  = []
+                                              , pSect2   = Nothing
+                                              }))
+    --copyFile index "d:\\stxt\\fhopecc\\www\\index.html"
+    --
 
 writeHtml :: (FilePath, Html) -> IO ()
 writeHtml (path, html) = do
@@ -67,97 +62,124 @@ writeHtml (path, html) = do
     hFlush f
     hClose f
 
-askHtmls :: Reader Env [(FilePath, Html)]
-askHtmls = do
-    ihtml <- askIndexHtml
-    s1htmls <- askSect1Htmls  
-    s2htmls <- askSect2Htmls
+
+getHtmls :: State Page [(FilePath, Html)]
+getHtmls = do
+    ihtml <- getIndexHtml
+    s1htmls <- getSect1Htmls  
+    s2htmls <- getSect2Htmls
     return $ ihtml : s1htmls ++ s2htmls
 
-setIndexPage :: State Page ()
-setIndexPage = do
-    s  <- get
-    (STXT.Doc t cs s1s) <- pDoc s
-    put s { pTitle   = t
+getIndexHtml :: State Page (FilePath, Html)
+getIndexHtml = do
+    p@(Page {pDoc = (STXT.Doc t cs s1s)})  <- get
+    put p { pTitle   = t
           , pContent = cs
           , pSect1   = Nothing
           , pSect2   = Nothing
           , pSect1s  = s1s
           , pSect2s  = []
           }
+    getPageHtml
 
-getSect1s = do
-    p <- get
-    return $ pSect1s p
+getSect1Htmls :: State Page [(FilePath, Html)]
+getSect1Htmls = do
+    p@(Page {pSect1s=s1s}) <- get
+    forM s1s $ \s1@(STXT.Sect1 n1 t cs s2s) -> do 
+        put p{ pContent = cs
+             , pSect1   = Just s1
+             , pSect2s  = s2s
+             , pSect2   = Nothing
+             }
+        getPageHtml
 
-getSect1 = do
-    p <- get
-    return $ pSect1 p  
-
-getSect2s = do
-    p <- get
-    return $ pSect2s p  
-
-getSect2 = do
-    p <- get
-    return $ pSect2 p
-
-getContent = do
-    p <- get
-    return $ pContent p  
-
-getTitle = do
-    p <- get
-    return $ pTitle p  
+getSect2Htmls :: State Page [(FilePath, Html)]
+getSect2Htmls = do
+    p@(Page {pSect1s=s1s}) <- get
+    s2s <- forM s1s $ \s1@(STXT.Sect1 _ _ _ s2s) -> do 
+        forM s2s $ \s2@(STXT.Sect2 n t cs) -> do
+            put p{ pContent = cs
+                 , pSect1 = Just s1
+                 , pSect2s = s2s
+                 , pSect2 = Just s2
+                 }
+            getPageHtml
+    return $ concat s2s
 
 getIndexFile :: State Page String
 getIndexFile = do
-    outdir <- getOutDir 
+    Page {pOutDir = outdir} <- get
     return $ outdir ++ "\\" ++ "index.html"
 
+getFilePath :: State Page String
+getFilePath = do
+    Page { pOutDir = outdir
+         , pDoc    = doc
+         , pSect1  = ss1
+         , pSect2  = ss2
+         } <- get
+
+    let f = if isJust ss2 then
+                sect2Path $ fromJust ss2
+            else if isJust ss1 then
+                    sect1Path $ fromJust ss1
+                 else "index.html"
+    return $ outdir ++ "\\" ++ f
+
 getTitleBar = do
-    title <- getTitle 
+    Page{pTitle = title} <- get
     return $ thediv ! [identifier "title_bar"] 
                 << table 
                     << besides [ td << anchor ! [href siteUrl] 
                                         << siteName  
-                               , td << title
+                               , td << anchor ! [href "index.html"] 
+                                        << title
                                ]
-
 
 getSect1Bar :: State Page Html
 getSect1Bar = do
-    s1s <- getSect1s
-    s1  <- getSect1
-    return $ if isJust s1 then do
-                let (STXT.Sect1 selected _ _ _) = fromJust s1
-                thediv ! [identifier "sect1_bar"] 
-                    << table 
-                        << besides [(label (n==selected)) << sect1Anchor s1 
-                                   | s1@(STXT.Sect1 n _ _ _) <- s1s
-                                   ]
-              else toHtml ""
+    Page { pSect1s = s1s
+         , pSect1  = ss1
+         } <- get
+    return $ if null s1s then toHtml ""
+             else thediv ! [identifier "sect1_bar"] << table 
+                      << if isJust ss1 then 
+                            let (STXT.Sect1 selected _ _ _) = fromJust ss1
+                            in besides [(label (n==selected)) << sect1Anchor s1 
+                                       | s1@(STXT.Sect1 n _ _ _) <- s1s
+                                       ]
+                         else
+                            besides [(label False) << sect1Anchor s1 
+                                     | s1 <- s1s
+                                     ]
+ 
 
 getSect2Bar :: State Page Html
 getSect2Bar = do
-    Page { pSect1  = s1
-         , pSect2s = s2s 
+    Page { pSect2s = s2s 
          , pSect2  = ss2
          } <- get
+    return $ if null s2s then toHtml ""
+             else thediv ! [identifier "sect2_bar"] << table
+                << if isJust ss2 then 
+                      let selected = fromJust ss2
+                      in aboves [ (label (s2 == selected )) << sect2Anchor s2 
+                                |  s2 <- s2s
+                                ]
+                      else
+                         aboves [(label False) << sect2Anchor s2
+                                | s2 <- s2s
+                                ]
 
-    return $ thediv ! [identifier "sect2_bar"] 
-                << table 
-                    << aboves [ (label (s2 == selected )) << sect2Anchor s2 
-                              | s2 <- s2s
-                              ]
 
 getPageHtml :: State Page (FilePath, Html)
 getPageHtml = do
-    title    <- getTitle
+    Page { pTitle = title
+         , pContent = content
+         } <- get
     titlebar <- getTitleBar
     sect1bar <- getSect1Bar 
     sect2bar <- getSect2Bar
-    content  <- getContent
     let html = thehtml ! [lang "zh-tw"] 
                 << header 
                     << myMeta
@@ -170,173 +192,8 @@ getPageHtml = do
                    +++ rightAds
                    +++ thediv ![identifier "content"]
                         << map elem2Html content
-    f <- getIndexFile
+    f <- getFilePath
     return (f, html)
-
-getIndexHtml :: Reader Env (FilePath, Html)
-getIndexHtml = do
-    t <- askDocTitle
-    doc@(STXT.Doc _ cs s1s) <- askDoc
-    titlebar <- askTitleBar 
-    sect1bar <- if null s1s then 
-                   return $ toHtml "" 
-                else askSect1Bar $ head s1s
-    sect2bar <- if null s1s then 
-                   return $ toHtml "" 
-                else do 
-                    let s1@(STXT.Sect1 _ _ _ s2s) = head s1s
-                    askSect2Bar (head s2s)
-
-    let html = thehtml ! [lang "zh-tw"] 
-                << header 
-                    << myMeta
-                   +++ myLink
-                   +++ thetitle << t
-               +++ body 
-                    << titlebar
-                   +++ sect1bar 
-                   +++ sect2bar 
-                   +++ rightAds
-                   +++ thediv ![identifier "content"]
-                        << map elem2Html cs
-    f <- askIndexFile
-    return (f, html)
-
-
-
-askIndexHtml :: Reader Env (FilePath, Html)
-askIndexHtml = do
-    t <- askDocTitle
-    doc@(STXT.Doc _ cs s1s) <- askDoc
-    titlebar <- askTitleBar 
-    sect1bar <- if null s1s then 
-                   return $ toHtml "" 
-                else askSect1Bar $ head s1s
-    sect2bar <- if null s1s then 
-                   return $ toHtml "" 
-                else do 
-                    let s1@(STXT.Sect1 _ _ _ s2s) = head s1s
-                    askSect2Bar (head s2s)
-
-    let html = thehtml ! [lang "zh-tw"] 
-                << header 
-                    << myMeta
-                   +++ myLink
-                   +++ thetitle << t
-               +++ body 
-                    << titlebar
-                   +++ sect1bar 
-                   +++ sect2bar 
-                   +++ rightAds
-                   +++ thediv ![identifier "content"]
-                        << map elem2Html cs
-    f <- askIndexFile
-    return (f, html)
-
-askSect1Htmls :: Reader Env [(FilePath, Html)]
-askSect1Htmls = do
-    s1s <- askSect1s
-    mapM askSect1Html s1s
-
-askSect2Htmls ::  Reader Env [(FilePath, Html)]
-askSect2Htmls = do
-    s1s <- askSect1s
-    mapM askSect2Html $ concat [s2s | s1@(STXT.Sect1 _ _ _ s2s) <- s1s]
-
-askSect1Html :: STXT.Sect1 -> Reader Env (FilePath, Html)
-askSect1Html s1@(STXT.Sect1 n t cs s2s) = do
-    doc@(STXT.Doc _ _ s1s) <- askDoc
-    titlebar <- askTitleBar
-    sect1bar <- askSect1Bar s1
-    sect2bar <- askSect2Bar (head s2s)
-    let html = thehtml ! [lang "zh-tw"] 
-                << header 
-                    << myMeta
-                   +++ myLink
-                   +++ thetitle << t
-               +++ body 
-                    << titlebar
-                   +++ sect1bar
-                   +++ sect2bar
-                   +++ rightAds
-                   +++ thediv ![identifier "content"]
-                        << map elem2Html cs
-
-    s1File <- askSect1File s1
-    return $ (s1File, html)
-
-askSect2Html :: STXT.Sect2 -> Reader Env (FilePath, Html)
-askSect2Html s2@(STXT.Sect2 (n1, n2) t cs) = do
-    doc@(STXT.Doc _ _ s1s) <- askDoc
-    s1 <- askSect1OfSect2 s2
-    titlebar <- askTitleBar
-    sect1bar <- askSect1Bar s1
-    sect2bar <- askSect2Bar s2
-    let html = thehtml ! [lang "zh-tw"] 
-                << header 
-                    << myMeta
-                   +++ myLink
-                   +++ thetitle << t
-               +++ body 
-                    << titlebar
-                   +++ sect1bar
-                   +++ sect2bar
-                   +++ rightAds
-                   +++ thediv ![identifier "content"]
-                        << map elem2Html cs
-    file <- askSect2File s2
-    return $ (file, html)
-
-askSect1OfSect2 :: STXT.Sect2 -> Reader Env STXT.Sect1
-askSect1OfSect2 s2@(STXT.Sect2 (n1, n2) _ _) = do
-    s1s <- askSect1s
-    return $ maybe (head s1s) id (find (\s1@(STXT.Sect1 n _ _ _) -> n == n1) s1s)
-
-
-askIndexFile :: Reader Env String
-askIndexFile = do
-    outdir <- askOutDir 
-    return $ outdir ++ "\\" ++ "index.html"
-
-askSect1File :: STXT.Sect1 -> Reader Env String
-askSect1File s1@(STXT.Sect1 n t _ _) = do
-    outdir <- askOutDir 
-    return $ outdir ++ "\\" ++ show n ++ ".html" 
-
-askSect2File :: STXT.Sect2 -> Reader Env String
-askSect2File s2@(STXT.Sect2 (n1, n2) t _) = do
-    outdir <- askOutDir 
-    return $ outdir ++ "\\" ++ sect2Path s2
-
-askTitleBar :: Reader Env Html
-askTitleBar = do
-    t <- askDocTitle 
-    return $ thediv ! [identifier "title_bar"] 
-                << table 
-                    << besides [ td << anchor ! [href siteUrl] 
-                                        << siteName  
-                               , td << t
-                               ]
-
-askSect1Bar :: STXT.Sect1 -> Reader Env Html
-askSect1Bar (STXT.Sect1 selected t _ _) = do
-    s1s <- askSect1s 
-    return $ thediv ! [identifier "sect1_bar"] 
-                << table 
-                    << besides [ (label (n==selected)) << sect1Anchor s1 
-                               | s1@(STXT.Sect1 n _ _ _) <- s1s
-                               ]
-
-askSect2Bar :: STXT.Sect2 -> Reader Env Html
-askSect2Bar s2@(STXT.Sect2 (sn1, sn2) _ _) = do
-    s1  <- askSect1OfSect2 s2
-    s2s <- askSect2s s1 
-    return $ thediv ! [identifier "sect2_bar"] 
-                << table 
-                    << aboves [ (label (n1 == sn1 && n2 == sn2 )) << sect2Anchor s2 
-                              | s2@(STXT.Sect2 (n1,n2) _ _) <- s2s
-                              ]
-
 
 sect1Anchor :: STXT.Sect1 -> Html
 sect1Anchor s1@(STXT.Sect1 n t _ _) = 
@@ -346,39 +203,12 @@ sect2Anchor :: STXT.Sect2 -> Html
 sect2Anchor s2@(STXT.Sect2 _ t _) = 
     anchor ! [href (sect2Path s2)] << t
 
-
 sect1Path :: STXT.Sect1 -> String
 sect1Path s1@(STXT.Sect1 n t _ _) = show n ++ ".html"
 
 sect2Path :: STXT.Sect2 -> String
 sect2Path s1@(STXT.Sect2 (n1, n2) t _) = 
     show n1 ++ "_" ++ show n2 ++ ".html"
-
-askOutDir :: Reader Env String
-askOutDir = do
-    env <- ask
-    return $ getOutDir env
-
-askDoc :: Reader Env STXT.Doc
-askDoc = do
-    env <- ask
-    case (getDoc env) of
-       doc@(STXT.Doc _ _ _) -> return $ doc
-       (STXT.Error msg)   -> error $ "Error happen:\n" ++ msg
-        
-askDocTitle :: Reader Env String
-askDocTitle = do 
-    (STXT.Doc t _ _) <- askDoc
-    return t
-
-askSect1s :: Reader Env STXT.Sect1s
-askSect1s = do 
-    (STXT.Doc _ _ s1s) <- askDoc
-    return s1s
-
-askSect2s :: STXT.Sect1 -> Reader Env STXT.Sect2s
-askSect2s (STXT.Sect1 _ _ _ s2s) = do 
-    return s2s
 
 elem2Html :: STXT.Elem -> Html
 elem2Html (STXT.Para ps) = thediv ! [theclass "para"]
