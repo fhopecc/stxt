9fe21bee890315ad2bc3acc6044affa4cc375104
unknown <fhopecc@gmail.com>
2014-04-05 22:54:30 +0800

    my wife's aunt's home in taipei! OK

--- config/vim/setvim.bat
+++ config/vim/setvim.bat
@@ -1,16 +1,14 @@
-rem
 rem 設定個人 VIM 環境
 rem
 
-set src=f:\stxt\config\vim
-set dst=C:\vim7\vim
+set src=%~p0
+set dst=C:\vim
 
-copy %src%\_vimrc %dst%
+copy %src%_vimrc %dst%
 
 if not exist %dst%\vimfiles mkdir %dst%\vimfiles
-
 copy %src%\format.vim %dst%\vimfiles
-
+ 
 if not exist %dst%\vimfiles\ftdetect mkdir %dst%\vimfiles\ftdetect
-
-copy %dst%\vimfiles\ftdetect\
+ 
+copy %src%\ftdetect\* %dst%\vimfiles\ftdetect\
