684b303da0a92864360a0eff39cfab8711887d1d
unknown <frankshen@.(none)>
2014-07-25 22:15:48 +0800

    create dos

--- config/vim/setvim.bat
+++ config/vim/setvim.bat
@@ -1,9 +1,17 @@
 rem 設定個人 VIM 環境
 rem
 
+goto %COMPUTERNAME%
+
+:FRANKSHEN-PC
+set src=%~dp0
+set dst=C:\vim
+goto copyfile
+
 set src=%~p0
 set dst=C:\vim
 
+:copyfile
 copy %src%_vimrc %dst%
 
 if not exist %dst%\vimfiles mkdir %dst%\vimfiles
@@ -12,3 +20,5 @@ copy %src%\format.vim %dst%\vimfiles
 if not exist %dst%\vimfiles\ftdetect mkdir %dst%\vimfiles\ftdetect
  
 copy %src%\ftdetect\* %dst%\vimfiles\ftdetect\
+:end
+echo "end"
