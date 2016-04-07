0d4ec5a2bacc1b9092658aa4f1a170008a547003
fhopecc <fhopecc@gmail.com>
2016-04-07 17:34:37 +0800

    abcd'

--- config/liu/bkliubox.bat
+++
@@ -1,7 +0,0 @@
-echo off
-if "%USERNAME%"=="JONE" (
-    rem JONE's Notebook
-    copy C:\ProgramData\Boshiamy\liu.box d:\stxt\config\liu
-) else (
-    copy %windir%\system32\liu.box d:\stxt\config\liu
-)
--- config/liu/setliubox.bat
+++
@@ -1,8 +0,0 @@
-rem 設定嘸蝦米片語
-rem
-
-set src=%~p0
-
-copy d:\stxt\config\liu\liu.box C:\ProgramData\Boshiamy\liu.box 
-
-pause
