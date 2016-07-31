0fa3b2f6d8cfc69b89a66a14d897cadbd3003143
fhopecc <fhopecc@gmail.com>
2016-07-31 22:06:05 +0800

    abcd

--- mobian/mobian.c
+++ mobian/mobian.c
@@ -1,7 +1,12 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
 
 int main(void){
-    printf("Hello!\n");
+    setlocale(LC_ALL, ".950");
+    char *mbs = (char*)malloc(50);
+    wcstombs(mbs, L"墨辨\n", 50);
+    printf(mbs);
     return 0;
 }
-
