0afc842e51c181f0ca291796cf4a1dca4fa8d4b4
fhopecc <fhopecc@gmail.com>
2016-08-28 15:28:01 +0800

    abcd

---
+++ mobian/dataobject.h
@@ -0,0 +1,8 @@
+#include <wchar.h>
+
+typedef struct {
+    w_char * wei;  
+    int zhi;
+    xiang * can;
+} han;
+
---
+++ mobian/test.c
@@ -0,0 +1,10 @@
+#include "dataobject.h"
+
+int main(void) {
+   // 員工(張簡稜剛,183,85,B,私立南台科技大學)
+   han h1 = { 
+    .wei=L"員工" 
+    .zhi=5
+    .can
+   };
+}
