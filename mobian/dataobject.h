2ebd69223fb319eeb22d66abe45515ec7988e9d9
fhopecc <fhopecc@gmail.com>
2016-09-02 21:32:49 +0800

    abcd

--- mobian/dataobject.h
+++ mobian/dataobject.h
@@ -1,8 +1,18 @@
 #include <wchar.h>
 
+typedef enum {
+    YUAN,
+    HAN 
+} xin; //型
+
 typedef struct {
-    w_char * wei;  
-    int zhi;
-    xiang * can;
-} han;
+    xin xin;    //型
+    void* zhi;  //值
+} xiang; //項
+
+typedef struct {
+    wchar_t * wei;  // 謂
+    int zhi;       // 枝
+    xiang * can;   // 參
+} han; //函
 
--- mobian/test.c
+++ mobian/test.c
@@ -1,10 +1,17 @@
+#include <stdio.h>
+#include <assert.h>
 #include "dataobject.h"
 
 int main(void) {
-   // 員工(張簡稜剛,183,85,B,私立南台科技大學)
-   han h1 = { 
-    .wei=L"員工" 
-    .zhi=5
-    .can
-   };
+    /*
+    // 員工(張簡稜剛,183,85,B,私立南台科技大學)
+    han h1 = { 
+        .wei=L"員工" 
+        .zhi=5
+        .can
+    };
+    */
+    xin a;
+    a = HAN;
+    assert(a==HAN);
 }
