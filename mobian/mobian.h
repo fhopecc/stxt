662227a17fa8c9e53dfaa8366ed29e7fc45e4328
fhopecc <fhopecc@gmail.com>
2016-09-13 16:29:36 +0800

    abcd

--- mobian/mobian.h
+++ mobian/mobian.h
@@ -1,38 +1,43 @@
 /* mobian.h
  *
  * 墨辨的基礎類型定義、錯誤處理
+ * 
  *  
  * 稜於花蓮美崙
  * 20160819 週五
  */
 #include <wchar.h>
-
-
 typedef wchar_t * str;
 
 typedef enum {
-    KONG=0,//項結構初始時，型會設成0，因此0保留給空
+    KONG=0,   //項結構初始時，型會設成0，因此0保留給空
     YUAN=1,
     HAN=2, 
     SHU=3
 } xin; //型
+
+/* 
+ * 項的值其處理由型去決定
+ * 目前分成 2 類，一是數字，二是指標指向資料儲存槽。
+ */
+typedef union {
+    double shu;
+    void * data;
+} zhi;  
 
 typedef struct {
     xin xin;    //型
     zhi zhi;  //值
 } xiang; //項
 
+/*
+ * 
+ */
 typedef struct {
     str wei;     // 謂
     int zhi;     // 枝
-    xiang * can; // 參
+    xiang ** can; // 參
 } han; //函
-
-typedef union {
-    double shu;
-    str ming;
-    han han;
-} zhi;  
 
 typedef enum {
     ALLOCATION_MEMROY_FAILED
