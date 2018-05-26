cdcdfad61803f581a95922ce314619bfcc9065c3
unknown <lgzhangjian@audit.tw>
2018-05-26 16:31:05 +0800

    modify aisimporter.py

--- audit/aisimporter.py
+++ audit/aisimporter.py
@@ -4,29 +4,23 @@ import sqlite3
 
 db = sqlite3.connect('ais.db')
 
-for fs in os.listdir():
-    print(fs)
+for f in [x for x in os.listdir() if x.endswith('.csv')]:
 
-def get_table_name(fname):
-    return fname.split('-')[2].split('.')[0]
+    with open(f, newline='', encoding = 'utf-8-sig') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        tn = f.split('-')[2].split('.')[0] 
+        isfirst = True
 
-f = '資料匯出-20180524090155-明細分類帳.csv'
-
-with open(f, newline='', encoding = 'utf-8-sig') as csvfile:
-    data = csv.reader(csvfile, delimiter=',')
-    tn = get_table_name(f)
-    isfirst = True
-
-    for row in data:
-        if isfirst:
-            # cn for column name
-            db.execute("create table %s (%s);" % 
-                      (tn, ' text, '.join(map(lambda cn: '`%s`'%cn, row))))
-            isfirst=False
-        else:
-            # v for value
-            db.execute("insert into %s values(%s)" % 
-                      (tn, ', '.join(map(lambda v: "'%s'"%v, row))))
+        for row in data:
+            if isfirst:
+                # cn for column name
+                db.execute("create table %s (%s);" % 
+                          (tn, ' text, '.join(map(lambda cn: '`%s`'%cn, row))))
+                isfirst=False
+            else:
+                # v for value
+                db.execute("insert into %s values(%s)" % 
+                          (tn, ', '.join(map(lambda v: "'%s'"%v, row))))
 
 db.commit()
 db.close()
